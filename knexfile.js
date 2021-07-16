b85f2869281ba4ff68c7c653353572fd5f6434c2
John Lednicky <john.d.lednicky@gmail.com>
2021-07-16 18:51:08 -0500

    Added cross-env
    cross-env solves problem with setting environment variables in scripts when working on both Windows and *x workstations.

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 const { knexSnakeCaseMappers } = require('objection');
 
 module.exports = {
-  development: {
+  production: {
     client: 'mysql2',
     connection: {
       database: process.env.LOCAL_MYSQL_DBNAME,
@@ -22,6 +22,27 @@ module.exports = {
     },
     ...knexSnakeCaseMappers(),
   },
+  development: {
+    client: 'mysql2',
+    connection: {
+      database: process.env.LOCAL_MYSQL_DBNAME,
+      user: process.env.LOCAL_MYSQL_USERNAME,
+      password: process.env.LOCAL_MYSQL_PASSWORD,
+      host: process.env.LOCAL_MYSQL_HOST,
+      connectionLimit: 10,
+      queueLimit: 10, /* pool property */
+      waitForConnections: true, /* pool property */
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+    ...knexSnakeCaseMappers(),
+  },  
   sqlite3: {
     client: 'sqlite3',
     connection: {
