a48dd9a050aea72afada70e2d6a511044683f62f
John Lednicky <john.d.lednicky@gmail.com>
2021-09-20 17:48:00 -0500

    gitflow-feature-stash: secrets-refactor
    Design of feature in comments of this file.

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,32 @@
 const { knexSnakeCaseMappers } = require('objection');
 
+/*
+How to get knex connection from async function
+https://stackoverflow.com/a/64980273/4628416
+
+const waitPort = require('wait-port');
+const fs = require('fs');
+
+const {
+    MYSQL_HOST: HOST,
+    MYSQL_HOST_FILE: HOST_FILE,
+    MYSQL_USER: USER,
+    MYSQL_USER_FILE: USER_FILE,
+    MYSQL_PASSWORD: PASSWORD,
+    MYSQL_PASSWORD_FILE: PASSWORD_FILE,
+    MYSQL_DB: DB,
+    MYSQL_DB_FILE: DB_FILE,
+} = process.env;
+
+  // these will read from the file first, otherwise the environment variable
+  const host = HOST_FILE ? fs.readFileSync(HOST_FILE) : HOST;
+  const user = USER_FILE ? fs.readFileSync(USER_FILE) : USER;
+  const password = PASSWORD_FILE ? fs.readFileSync(PASSWORD_FILE) : PASSWORD;
+  const database = DB_FILE ? fs.readFileSync(DB_FILE) : DB;
+
+  await waitPort({ host, port : 3306}); //this blocks until the port is available
+*/
+
 module.exports = {
   production: {
     client: 'mysql2',
